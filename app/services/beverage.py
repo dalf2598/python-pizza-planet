1816a5e2b71d887edbf9d8365ec49df2872deaa1
dalf2598 <alejofon98@hotmail.com>
2023-08-15 18:31:54 -0500

    refactor: apply strategy pattern to refactor services

--- app/services/beverage.py
+++ app/services/beverage.py
@@ -1,36 +1,25 @@
 from app.common.http_methods import GET, POST, PUT
-from flask import Blueprint, jsonify, request
+from app.services.base import BaseService
+from flask import Blueprint, request
 
 from ..controllers import BeverageController
 
 beverage = Blueprint('beverage', __name__)
 
+beverage_service = BaseService(controller=BeverageController)
+
 @beverage.route('/', methods=POST)
 def create_size():
-    size, error = BeverageController.create(request.json)
-    response = size if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
-
+    return beverage_service.create(request)
 
 @beverage.route('/', methods=PUT)
 def update_size():
-    size, error = BeverageController.update(request.json)
-    response = size if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
-
+    return beverage_service.update(request)
 
 @beverage.route('/id/<_id>', methods=GET)
 def get_size_by_id(_id: int):
-    size, error = BeverageController.get_by_id(_id)
-    response = size if not error else {'error': error}
-    status_code = 200 if size else 404 if not error else 400
-    return jsonify(response), status_code
+    return beverage_service.get_by_id(_id)
 
 @beverage.route('/', methods=GET)
 def get_sizes():
-    sizes, error = BeverageController.get_all()
-    response = sizes if not error else {'error': error}
-    status_code = 200 if sizes else 404 if not error else 400
-    return jsonify(response), status_code
+    return beverage_service.get_all()
